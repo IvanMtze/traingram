ae9d598fd0e7ac3bed233478e0089492eec9802b
gerardo espinosa <ivanmtze96@gmail.com>
2021-03-14 21:51:10 -0600

    Added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__header">
+        <img className="app__header__img"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
       <h1>Hello from instagram clone</h1>
+
       {/* Header*/}
       {/* Post*/}
       {/* Post*/}
